17abe8547bc349c510c56c530c1475e9eed1f24b
Ramon Ziai <ramon.ziai@uni-tuebingen.de>
2020-06-25 16:36:29 +0200

    conversion of Stanza annotation to DKPro types for UIMA compatibility

--- src/main/java/de/lingmod/stanza/client/StanzaAPIClient.java
+++ src/main/java/de/lingmod/stanza/client/StanzaAPIClient.java
@@ -1,5 +1,6 @@
 package de.lingmod.stanza.client;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.client.Client;
@@ -9,8 +10,16 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.uima.cas.CASException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
 import de.lingmod.stanza.data.AnalysisRequest;
 import de.lingmod.stanza.data.TokenOrWord;
+import de.lingmod.stanza.util.AnnotationUtils;
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 
 public class StanzaAPIClient {
 
@@ -30,9 +39,17 @@ public class StanzaAPIClient {
 		.post(Entity.entity(req, MediaType.APPLICATION_JSON), new GenericType<List<List<TokenOrWord>>>() {});
 	}
 	
-	public static void main(String[] args) {
-		StanzaAPIClient client = new StanzaAPIClient("http://localhost:8000/analyze");
-		List<List<TokenOrWord>> result = client.analyze("Im Leben läuft manches anders, als man denkt.", "de");
+	public static void main(String[] args) throws ResourceInitializationException, CASException, IOException {
+		StanzaAPIClient client = new StanzaAPIClient("http://rinia.sfs.uni-tuebingen.de:8000/analyze");
+		String inputText = "Im Leben läuft manches anders, als man denkt. Hier ist noch ein Satz.";
+		String langCode = "de";
+		List<List<TokenOrWord>> result = client.analyze(inputText, langCode);
 		System.out.println(result);
+		
+		JCas jcas = JCasFactory.createJCas();
+		AnnotationUtils.annotateDkPro(inputText, langCode, result, jcas);
+		for (Dependency d : JCasUtil.select(jcas, Dependency.class)) {
+			System.out.println(d);
+		}
 	}
 }
